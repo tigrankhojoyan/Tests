c50eec342967d04aa7a8449a7cae253bc0a55c07
Hasmik Dabaghyan <hasmik_d@instigatemobile.com>
2015-06-23 15:24:19 +0400

    - Corrected functionality of sending email with attached file

--- Tests/src/main/java/testng/tests/MailSender.java
+++ Tests/src/main/java/testng/tests/MailSender.java
@@ -5,14 +5,21 @@
  */
 package testng.tests;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  *
@@ -23,7 +30,7 @@ public class MailSender {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         // Recipient's email ID needs to be mentioned.
         final String username = "tigrankhojoyan@gmail.com";
         final String password = "TKH0405tkh";
@@ -41,7 +48,7 @@ public class MailSender {
         try {
 
             Message message = new MimeMessage(session);
-            message.setFileName("src/main/resources/testFile.txt");
+            message.setFileName("src/main/resources/testFile1.txt");
             message.setFrom(new InternetAddress("tigrankhojoyan@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse("tigrankhojoyan@mail.ru"));
@@ -55,6 +62,95 @@ public class MailSender {
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
+        }
+    }*/
+     public static void sendEmailWithAttachments(String host, String port,
+            final String userName, final String password, String toAddress,
+            String subject, String message, String[] attachFiles)
+            throws AddressException, MessagingException, IOException {
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.user", userName);
+        properties.put("mail.password", password);
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+ 
+        // creates message part
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(message, "text/html");
+ 
+        // creates multi-part
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+ 
+        // adds attachments
+        if (attachFiles != null && attachFiles.length > 0) {
+            for (String filePath : attachFiles) {
+                MimeBodyPart attachPart = new MimeBodyPart();
+ 
+                try {
+                    attachPart.attachFile(filePath);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+ 
+                multipart.addBodyPart(attachPart);
+            }
+        }
+ 
+        // sets the multi-part as e-mail's content
+        msg.setContent(multipart);
+ 
+        // sends the e-mail
+        Transport.send(msg);
+ 
+    }
+ 
+    /**
+     * Test sending e-mail with attachments
+     */
+    public static void main(String[] args) {
+        // SMTP info
+        String host = "smtp.gmail.com";
+        String port = "587";
+        String mailFrom = "tigrankhojoyan@gmail.com";
+        String password = "TKH0405tkh";
+ 
+        // message info
+        String mailTo = "tigrankhojoyan@mail.ru";
+        String subject = "New email with attachments";
+        String message = "I have some attachments for you.";
+ 
+        // attachments
+        String[] attachFiles = new String[1];
+        attachFiles[0] = "src/main/resources/testFile.txt";
+ 
+        try {
+            sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+                subject, message, attachFiles);
+            System.out.println("Email sent.");
+        } catch (Exception ex) {
+            System.out.println("Could not send email.");
+            ex.printStackTrace();
         }
     }
 }
